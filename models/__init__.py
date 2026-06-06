cf9795a5f37a4fe2b893f82f2c6a09ae1e73d152
BdRjo <baderaq@gmail.com>
2026-06-06 21:06:21 +0300

    feat: add HRS models as separate file

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .database import (Base, Role, Permission, RolePermission, User,
                         Location, Venue, Reservation, BlockedPeriod,
                         SystemLog, LoginLog, get_engine, get_session)
+
+from .hrs_models import *
